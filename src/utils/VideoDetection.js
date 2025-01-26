addfba91ed921578573f85d69fad2f54398f9eb7
Mohamed Elamine Bengueddache <142161809+bengmoh@users.noreply.github.com>
2025-01-26 10:18:41 -0500

    Add support for Coursera lecture videos

--- src/utils/VideoDetection.js
+++ src/utils/VideoDetection.js
@@ -2,32 +2,40 @@
 
 const SUPPORTED_PLATFORMS = ['www.youtube.com','www.netflix.com', 'www.primevideo.com', 'www.disneyplus.com'];
 
+const firstHandlingCheckers = [youtube, netflix, disneyPlus, coursera];
+const secondHandlingCheckers = [primeVideo];
+
+/**
+ * Check if the URL is a video player URL
+ * @param {string} url - The URL to check
+ * @returns {number} - 0: no video player, 1: first handling platform, 2: second handling platform, -1: iframe
+ */
 export function isVideoPlayerURL(url) {
-    if (isYouTube(url) || isNetflix(url) || isDisneyPlus(url)) {
-      return 1;
-    }
+    if (firstHandlingCheckers.some(checker => checker(url))) return 1;
     
-    else if (isPrimeVideo(url)) {
-      return 2;
-    }
+    if (secondHandlingCheckers.some(checker => checker(url))) return 2;
     
-    // If not supported platform, handle it as iframe detection (3) 
-    return SUPPORTED_PLATFORMS.includes(url.split('/')[2]) ? 0 : 3; 
+    url = url.split('/')[2]; // Get the domain
+    return SUPPORTED_PLATFORMS.includes(url) ? 0 : -1; // If the domain is in the supported platforms, return 0 (no video player), else return -1 (iframe)
 }
 
 
-function isYouTube(url) {
+function youtube(url) {
     return url.includes('youtube.com/watch') || url.includes('youtube.com/shorts');
 }
 
-function isNetflix(url) {
+function netflix(url) {
     return url.includes('netflix.com/watch') && !url.includes('miniDpPlayButton');
 }
 
-function isDisneyPlus(url) {
+function disneyPlus(url) {
     return url.includes('disneyplus.com') && url.includes('play');
 }
 
-function isPrimeVideo(url) {
+function coursera(url) {
+    return url.includes('coursera.org') && url.includes('lecture');
+}
+
+function primeVideo(url) {
     return url.includes('primevideo.com') && url.includes('detail');
 }
